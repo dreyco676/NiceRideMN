e6ae1f0e81da9e5d615097b15efb03eea1914c0c
dreyco676 <dreyco676@gmail.com>
2015-07-23 08:21:47 -0500

    captured weather data

--- get_weather_data.py
+++ get_weather_data.py
@@ -2,15 +2,15 @@ import weather
 from datetime import datetime
 from datetime import timedelta
 
-
+# 2014-11-26T00:00,2014-11-26T23:59
 wc = weather.WeatherClient()
 
 # first day to get data
-start_dt = datetime.strptime('2010-06-10T00:00','%Y-%m-%dT%H:%M')
+start_dt = datetime.strptime('2014-11-26T00:00', '%Y-%m-%dT%H:%M')
 # end of first day to get data
-eod_start_dt = datetime.strptime('2010-06-10T23:59','%Y-%m-%dT%H:%M')
+eod_start_dt = datetime.strptime('2014-11-26T23:59', '%Y-%m-%dT%H:%M')
 # last day to get data
-end_dt = datetime.strptime('2015-07-01','%Y-%m-%d') + timedelta(days=1)
+end_dt = datetime.strptime('2015-07-01', '%Y-%m-%d') + timedelta(days=1)
 
 while eod_start_dt < end_dt:
     time_st = start_dt.strftime('%Y-%m-%dT%H:%M') + ',' + eod_start_dt.strftime('%Y-%m-%dT%H:%M')
